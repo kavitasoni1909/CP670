3bf88ea380bf074daf9efd3b967ca081faedc93c
kavit <kavitasoni1909@gmail.com>
2019-09-22 13:47:07 -0400

    Assignment2 part2

--- app/src/main/java/com/example/androidassignments/MainActivity.java
+++ app/src/main/java/com/example/androidassignments/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.androidassignments;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected static final String ACTIVITY_NAME = "Main Activity";
+
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(ACTIVITY_NAME, "In on Create");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i(ACTIVITY_NAME, "In on Resume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i(ACTIVITY_NAME, "In on Start");
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i(ACTIVITY_NAME, "In on Pause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(ACTIVITY_NAME, "In on Stop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(ACTIVITY_NAME, "In on Destroy");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 10 && resultCode == Activity.RESULT_OK) {
+            Log.i(ACTIVITY_NAME, "Returned to Main Activity.onActivityResult");
+            String messagePassed = data.getStringExtra("Response");
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(MainActivity.this, messagePassed, duration);
+            toast.show();
+        }
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, ListItemsActivity.class);
+        startActivityForResult(intent, 10);
     }
 }
